0ae9f8b86e79a4f29d5555857ade778426604fdd
edvard01 <frejaclueless@gmail.com>
2024-01-12 14:12:33 +0100

    All exercises done until the last one. Gonna take a look at objects and do it when I feel comfortable.

--- js/index.js
+++ js/index.js
@@ -49,3 +49,61 @@ function kmToMile(km) {
     return "No valid input";
   }
 }
+
+function celcToFahr(temp) {
+  if (!isNaN(temp)) {
+    return temp * 1.8 + 32;
+  } else {
+    return "no valid input";
+  }
+}
+
+function meanVal(array) {
+  let sum = 0;
+  array.forEach((element) => {
+    if (!isNaN(element)) {
+      sum += element;
+    }
+  });
+
+  return sum / array.length;
+}
+
+let arr = [5, 15];
+
+console.log(meanVal(arr));
+
+function bolaget(age) {
+  if (age < 15) {
+    return "Get out of here!";
+  } else if (age > 14 && age < 18) {
+    return "Sorry, you are not old enough!";
+  } else if (age > 17 && age < 20) {
+    return "Sorry, you must be at least 20. Why don't you try a bar instead?";
+  } else if (age > 19 && age < 81) {
+    return "Thank you! Welcome!";
+  } else if (age > 80) {
+    return "I'm sorry! Where is your caretaker?";
+  } else {
+    return "You do not exist...";
+  }
+}
+
+function sumOfTen(arr) {
+  if (arr.length !== 10) {
+    return "Array must be 10 values long!";
+  }
+
+  let sum = 0;
+  arr.forEach((element) => {
+    if (!isNaN(element)) {
+      sum += element;
+    }
+  });
+
+  return sum;
+}
+
+let sumArray = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+console.log(sumOfTen(sumArray));
